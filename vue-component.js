9d1da3a1c05e0d20e997440eb8544b6fcf8a5073
Julien Bouvet <julien.bouvet@ekino.com>
2017-08-01 11:26:08 +0200

    Updating code style

--- vue-component.js
+++ vue-component.js
@@ -1,73 +1,69 @@
-'use strict';
-import Vue from 'vue';
-import Case from 'case';
+'use strict'
+import Vue from 'vue'
+import Case from 'case'
 
 let VueComponent = function() {
 	const PROPERTY_RADIX = 'data-component--',
 		REGEXP_PROPERTY = new RegExp(`^${PROPERTY_RADIX}`),
-		VUE_EVENT_BUS = new Vue();
+		VUE_EVENT_BUS = new Vue()
 
 	let $mainVue = null,
 		$templateNames = [],
 		$templateProperties = [],
 		$templateCache = [],
 		_promises = [],
-		$parser = new DOMParser();
+		$parser = new DOMParser()
 
 	// utils
-	let generateUID = (name = '', a = '', b = '') => {
-		while (a < 12) {
-			b += (a * 51) & 52
-				? (a ^ 15 ? 8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) : 4).toString(16)
-				: '-';
-			a++;
-		}
-		return `${name}${name.length > 0 ? '--' : ''}${b}`;
-	},
+	let generateUID = (name = '', a = 0, b = '') => {
+			while (a < 12) {
+				b +=
+					(a * 51) & 52
+						? (a ^ 15 ? 8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) : 4).toString(
+								16
+							)
+						: '-'
+				a++
+			}
+			return `${name}${name.length > 0 ? '--' : ''}${b}`
+		},
 		trimValues = (value = '') => `${value}`.trim(),
 		concat = function(...list) {
-			return list.map(trimValues).join(' ');
+			return list.map(trimValues).join(' ')
 		},
 		// hooks
 		getMainVue = () => $mainVue,
 		getEventBus = () => VUE_EVENT_BUS,
 		// build
 		register = function(components, options = null) {
-			if (Array.isArray(components) === true) {
-				for (let component of components) {
-					_promises.push(registerOneComponent(`${component}`));
-				}
-			} else {
-				_promises.push(registerOneComponent(`${components}`));
-			}
-			return Promise.all(_promises)
+			const PROMISES = []
+				.concat(components)
+				.map(component => registerOneComponent(`${component}`))
+			return Promise.all(PROMISES)
 				.then(_templates => {
 					for (let _html of _templates) {
-						let { _template, _props } = getTemplateProperties(_html);
-						prepare(`${_template}`, _props);
+						let { _template, _props } = getTemplateProperties(_html)
+						prepare(`${_template}`, _props)
 					}
 				})
-				.then(() => (options !== null ? set(options) : null));
+				.then(() => (options !== null ? set(options) : null))
 		},
 		isolateComponentName = function(componentName) {
 			if (/\?noscript$/i.test(componentName) === true) {
-				return [
-					componentName.replace(/^(.*)\?noscript$/i, '$1'),
-					'?noscript=1'
-				];
+				return [componentName.replace(/^(.*)\?noscript$/i, '$1'), '?noscript=1']
 			} else {
-				return [componentName, ''];
+				return [componentName, '']
 			}
 		},
 		registerOneComponent = function(componentName) {
 			let _splitComponentName = isolateComponentName(componentName),
 				$index = $templateNames.findIndex(
 					(name, index) => name === _splitComponentName[0]
-				);
+				)
 			if ($index === -1) {
-				return fetchTemplate(_splitComponentName[0], _splitComponentName[1]);
+				return fetchTemplate(_splitComponentName[0], _splitComponentName[1])
 			} else {
-				return build($templateCache[$index], name, $templateProperties[$index]);
+				return build($templateCache[$index], name, $templateProperties[$index])
 			}
 		},
 		fetchTemplate = (componentName, noscript = '') =>
@@ -82,53 +78,51 @@ let VueComponent = function() {
 					name: '',
 					properties: [],
 					methods: {}
-				};
+				}
 
 			for (let _prop of Array.from(_target.attributes)) {
 				if (_prop != 'prototype' && _prop != 'length') {
 					if (REGEXP_PROPERTY.test(_prop.name) === true) {
-						_propertyName = _prop.name.substring(PROPERTY_RADIX.length);
+						_propertyName = _prop.name.substring(PROPERTY_RADIX.length)
 						if (_propertyName === 'name') {
-							_props.name = _prop.value;
+							_props.name = _prop.value
 						} else {
-							_props.properties.push(Case.toCamelCase(_propertyName));
+							_props.properties.push(Case.toCamelCase(_propertyName))
 						}
 					}
 				}
 			}
-			_scriptNode = _node !== null
-				? _node.getElementsByTagName('script')
-				: null;
+			_scriptNode = _node !== null ? _node.getElementsByTagName('script') : null
 			if (_scriptNode && _scriptNode[0]) {
 				_props.methods = eval(_scriptNode[0].innerHTML).make(
 					getMainVue,
 					getEventBus
-				);
+				)
 			}
-			return { _template: _target.outerHTML, _props };
+			return { _template: _target.outerHTML, _props }
 		},
 		prepare = function(template, _props) {
-			$templateNames.push(_props.name);
-			$templateProperties.push(_props.properties);
-			$templateCache.push(template);
-			build(template, _props.name, _props.properties, _props.methods);
+			$templateNames.push(_props.name)
+			$templateProperties.push(_props.properties)
+			$templateCache.push(template)
+			build(template, _props.name, _props.properties, _props.methods)
 		},
 		build = function(template, name, props = {}, methods = {}) {
-			Object.assign(methods, { generateUID, concat }); // utilities
+			Object.assign(methods, { generateUID, concat }) // utilities
 			let _component = Vue.extend({
 				template,
 				props,
 				methods,
 				data: () => ({ uniqueId: generateUID(name) })
-			});
-			Vue.component(name, _component);
+			})
+			Vue.component(name, _component)
 		},
 		set = function(options) {
 			try {
-				return new Vue(options);
+				return new Vue(options)
 			} catch (err) {
-				console.error(err);
-				return null;
+				console.error(err)
+				return null
 			}
 		},
 		addEventListener = (_eventName, _callback) =>
@@ -136,15 +130,15 @@ let VueComponent = function() {
 		fireEvent = (_eventName, ...args) =>
 			VUE_EVENT_BUS.$emit(_eventName, ...args),
 		track = function(_vue) {
-			$mainVue = _vue;
+			$mainVue = _vue
 			$mainVue.$nextTick(function() {
 				for (let component of $mainVue.$children) {
-					if (component.init) component.init();
+					if (component.init) component.init()
 				}
-			});
-		};
+			})
+		}
 
-	return { register, set, addEventListener, fireEvent, track, generateUID };
-};
+	return { register, set, addEventListener, fireEvent, track, generateUID }
+}
 
-module.exports = new VueComponent();
+module.exports = new VueComponent()
